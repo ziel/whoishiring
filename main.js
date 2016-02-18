181fd013d7e428dcefe9a5be7d492ff17a52253c
ziel <ziel@traveling>
2016-02-17 19:22:32 -0500

    Make full width separator. Get width from tty, if applicable.

--- main.js
+++ main.js
@@ -7,16 +7,6 @@ const Format = require('./lib/format')
 const Output = require('./lib/output')
 const Promise = require('bluebird')
 
-// Add a newline to a string
-//
-// @param {string} str
-// The string to add to
-//
-// @return {string}
-// The given string with a newline appended
-//
-const addNewline = (str) => str + '\n'
-
 // The search terms to use in the query
 // (provided by the user)
 //
@@ -27,15 +17,16 @@ const terms = Format.args()
 Promise.coroutine(function *() {
   Output.progress('Finding latest story...')
 
-  let header = [
-    addNewline(yield WhosHiring.url()),
-    addNewline(yield WhosHiring.title())
+  let meta = [
+    yield WhosHiring.url(),
+    yield WhosHiring.title()
   ]
 
   Output.progress(`Finding matches for: ${terms}`)
 
   let matches = yield WhosHiring.matches(terms)
   let results = matches.map((r) => Format.html(r))
+  let header = meta.join('\n')
 
-  Output.data(header.concat(results).join(''))
+  Output.data(header + results.join(''))
 })()
